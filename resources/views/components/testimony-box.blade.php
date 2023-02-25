55c7270625b75004aa71b528cb6680f63e6a8669
Alvin Leonardo <alvinvin00@outlook.com>
2023-02-25 21:46:22 +0700

    feat: Gallery

---
+++ resources/views/components/testimony-box.blade.php
@@ -0,0 +1,54 @@
+<div class="py-4 bg-primary bg-opacity-50 text-white"
+        {{--         style="background-image: url('{{asset('assets/images/bg-testimony.png')}}'); background-position: center; background-repeat: no-repeat"--}}
+>
+    <h2 class="text-uppercase text-center">Testimonials</h2>
+    <div class="container-lg mx-auto row gap-4">
+        <div class="col bg-primary bg-opacity-75 p-2 rounded-3 d-flex flex-column">
+            <div class="d-flex gap-2 align-items-center">
+                <img src="https://via.placeholder.com/50" alt="Picture of Person Name"
+                     class="img-fluid rounded-circle border-white border-2" height="50" width="50">
+                <b>
+                    Test
+                </b>
+            </div>
+            <hr>
+            <p class="text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, debitis dolor earum fuga id impedit
+                ipsam laboriosam laudantium magnam magni maiores minima, nemo nesciunt quae quaerat quos recusandae
+                vero! Quibusdam?
+            </p>
+        </div>
+        <div class="col bg-primary bg-opacity-75 p-2 rounded-3 d-flex flex-column">
+            <div class="d-flex gap-2 align-items-center">
+                <img src="https://via.placeholder.com/50" alt="Picture of Person Name"
+                     class="img-fluid rounded-circle border-2 border-white" height="50" width="50">
+                <b>
+                    Test
+                </b>
+            </div>
+            <hr>
+            <p class="text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, debitis dolor earum fuga id impedit
+                ipsam laboriosam laudantium magnam magni maiores minima, nemo nesciunt quae quaerat quos recusandae
+                vero! Quibusdam?
+            </p>
+        </div>
+        <div class="col bg-primary bg-opacity-75 p-2 rounded-3 d-flex flex-column">
+            <div class="d-flex gap-2 align-items-center">
+                <div class="rounded-circle border-white border-2">
+                    <img src="https://via.placeholder.com/50" alt="Picture of Person Name"
+                         class="img-fluid rounded-circle border-white border-2" height="50" width="50">
+                </div>
+                <b>
+                    Test
+                </b>
+            </div>
+            <hr class="border-white border-2">
+            <p class="text-muted">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, debitis dolor earum fuga id impedit
+                ipsam laboriosam laudantium magnam magni maiores minima, nemo nesciunt quae quaerat quos recusandae
+                vero! Quibusdam?
+            </p>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/unit-info.blade.php
@@ -0,0 +1,71 @@
+<div class="container-lg my-2">
+    <h2 class="text-uppercase text-center">
+        Informasi Unit Satuan Kerja
+    </h2>
+    <div class="row gap-lg-2 flex-lg-nowrap">
+        <div class="col col-6 col-md-3 d-flex flex-column text-center gap-3 align-items-center">
+            <div class="rounded-circle p-3 text-primary border border-primary border-2 w-50 w-lg-25">
+                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor"
+                     class="bi bi-folder2-open" viewBox="0 0 16 16">
+                    <path
+                            d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14V3.5zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5V6zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7H1.633z"/>
+                </svg>
+            </div>
+            <b class="h4 text-uppercase">
+                Informasi Prosedur Layanan
+            </b>
+            <p>
+                Berisi seluruh informasi procedure layanan yang rnendasari pelaksanoan tugas pokok dan fungsi
+                Lembaga Pemasyarakatan
+            </p>
+        </div>
+        <div class="col col-6 col-md-3 d-flex flex-column text-center gap-3 align-items-center">
+            <div class="rounded-circle p-3 text-primary border border-primary border-2 w-50 w-lg-25">
+                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor"
+                     class="bi bi-database" viewBox="0 0 16 16">
+                    <path
+                            d="M4.318 2.687C5.234 2.271 6.536 2 8 2s2.766.27 3.682.687C12.644 3.125 13 3.627 13 4c0 .374-.356.875-1.318 1.313C10.766 5.729 9.464 6 8 6s-2.766-.27-3.682-.687C3.356 4.875 3 4.373 3 4c0-.374.356-.875 1.318-1.313ZM13 5.698V7c0 .374-.356.875-1.318 1.313C10.766 8.729 9.464 9 8 9s-2.766-.27-3.682-.687C3.356 7.875 3 7.373 3 7V5.698c.271.202.58.378.904.525C4.978 6.711 6.427 7 8 7s3.022-.289 4.096-.777A4.92 4.92 0 0 0 13 5.698ZM14 4c0-1.007-.875-1.755-1.904-2.223C11.022 1.289 9.573 1 8 1s-3.022.289-4.096.777C2.875 2.245 2 2.993 2 4v9c0 1.007.875 1.755 1.904 2.223C4.978 15.71 6.427 16 8 16s3.022-.289 4.096-.777C13.125 14.755 14 14.007 14 13V4Zm-1 4.698V10c0 .374-.356.875-1.318 1.313C10.766 11.729 9.464 12 8 12s-2.766-.27-3.682-.687C3.356 10.875 3 10.373 3 10V8.698c.271.202.58.378.904.525C4.978 9.71 6.427 10 8 10s3.022-.289 4.096-.777A4.92 4.92 0 0 0 13 8.698Zm0 3V13c0 .374-.356.875-1.318 1.313C10.766 14.729 9.464 15 8 15s-2.766-.27-3.682-.687C3.356 13.875 3 13.373 3 13v-1.302c.271.202.58.378.904.525C4.978 12.71 6.427 13 8 13s3.022-.289 4.096-.777c.324-.147.633-.323.904-.525Z"/>
+                </svg>
+            </div>
+            <b class="h4 text-uppercase">
+                Informasi Publik
+            </b>
+            <p>
+                Berisi informasi tentang seluruh kegiatan program pembinaan WBP yang dilaksanakan pada I-Init
+                Pelaksana Teknis Pemasyarakatan
+            </p>
+        </div>
+        <div class="col col-6 col-md-3 d-flex flex-column text-center gap-3 align-items-center">
+            <div class="rounded-circle p-3 text-primary border border-primary border-2 w-50 w-lg-25">
+                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor"
+                     class="bi bi-people-fill" viewBox="0 0 16 16">
+                    <path
+                            d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
+                </svg>
+            </div>
+            <b class="h4 text-uppercase">
+                Program Pembinaan
+            </b>
+            <p>
+                Berisi Data Unit Pelaksana Teknis Pemasyarakatan di seluruh Indonesia yang tersimpan dalam Sistem
+                Database pemasyarakatan
+            </p>
+        </div>
+        <div class="col col-6 col-md-3 d-flex flex-column text-center gap-3 align-items-center">
+            <div class="rounded-circle p-3 text-primary border border-primary border-2 w-50 w-lg-25">
+                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor"
+                     class="bi bi-basket-fill" viewBox="0 0 16 16">
+                    <path
+                            d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717L5.07 1.243zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3z"/>
+                </svg>
+            </div>
+            <b class="h4 text-uppercase">
+                Hasil karya WBP
+            </b>
+            <p>
+                Berisi informasi tentang hasil karya Warga Binaan Pemasyarakatan yang terdapat pada tiap unit
+                Pelaksana Teknis Pemasyarakatan
+            </p>
+        </div>
+    </div>
+</div>
