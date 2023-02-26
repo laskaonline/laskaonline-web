5996044ee3dafdd29e8566e8b03322c4a5470cb6
Alvin Leonardo <alvinvin00@outlook.com>
2023-02-26 20:37:20 +0700

    feat: Landing Page

---
+++ resources/views/components/gallery.blade.php
@@ -0,0 +1,10 @@
+<div class="container-lg my-2">
+    <h3 class="text-center text-uppercase">Galeri Laksa</h3>
+    <div class="grid">
+        <div class="g-col-6 g-col-md-4 g-col-lg-3 bg-primary text-white">1</div>
+        <div class="g-col-6 g-col-md-4 g-col-lg-3 bg-primary text-white">1</div>
+        <div class="g-col-6 g-col-md-4 g-col-lg-3 bg-primary text-white">1</div>
+        <div class="g-col-6 g-col-md-4 g-col-lg-3 bg-primary text-white">1</div>
+        <div class="g-col-6 g-col-md-4 g-col-lg-3 bg-primary text-white">1</div>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,36 @@
     <img src="{{asset('/assets/images/personnel.png')}}" alt="Personnel" class="img-fluid mx-auto d-block">
     <x-unit-info/>
     <x-testimony-box/>
-    <div class="my-2 container-lg">
-        <h2 class="text-center text-uppercase">Galeri Laksa</h2>
-        <div class="grid">
-            <div class="g-col-6 g-col-md-4 g-col-lg-3 bg-primary text-white">1</div>
-            <div class="g-col-6 g-col-md-4 g-col-lg-3 bg-primary text-white">1</div>
-            <div class="g-col-6 g-col-md-4 g-col-lg-3 bg-primary text-white">1</div>
-            <div class="g-col-6 g-col-md-4 g-col-lg-3 bg-primary text-white">1</div>
-            <div class="g-col-6 g-col-md-4 g-col-lg-3 bg-primary text-white">1</div>
-        </div>
+    <x-gallery/>
+    <div class="container-lg my-2">
+        <h3 class="text-uppercase text-center">
+            Form Saran dan Masukan
+        </h3>
+        <form action="/" method="post" class="d-flex flex-column gap-2">
+            <x-honeypot/>
+            <div>
+                <label class="form-label">Nama (required)</label>
+                <input name="full_name" class="form-control">
+            </div>
+            <div>
+                <label class="form-label">Email (required)</label>
+                <input name="email" type="email" class="form-control">
+            </div>
+            <div>
+                <label class="form-label">Pesan</label>
+                <textarea name="message" class="form-control" rows="3"></textarea>
+            </div>
+            <button class="btn btn-primary w-100 w-lg-auto" type="submit">
+                Submit
+            </button>
+        </form>
     </div>
-
+    <footer class="container-fluid py-2 bg-primary text-white d-flex align-items-center">
+        <div class="d-flex gap-2">
+            <i class="bi bi-facebook"></i>
+            <i class="bi bi-twitter"></i>
+            <i class="bi bi-youtube"></i>
+        </div>
+        <span class="mx-auto">&copy; Laksa 2023</span>
+    </footer>
 @endsection
