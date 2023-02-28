9b7919d72e635e20574bd75a98dfc69303f5398d
Alvin Leonardo <alvinvin00@outlook.com>
2023-02-28 21:27:11 +0700

    chore: Seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,11 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
@@ -20,19 +21,14 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'phone' => fake('id_ID')->phoneNumber(),
+            'gender' => 'Laki-Laki',
+            'address' => fake('id_ID')->streetAddress(),
+            'no_ktp' => fake('id_ID')->nik(),
+            'photo' => fake()->imageUrl(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,12 +2,28 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class AdminSeeder extends Seeder
 {
     public function run(): void
     {
+        $superAdmin = User::factory()->create([
+            'email' => 'superadmin@test.com'
+        ]);
 
+        $superAdmin->assignRole('superior');
+
+        $admins = User::factory(3)->state(new Sequence(
+            ['email' => 'admin1@test.com'],
+            ['email' => 'admin2@test.com'],
+            ['email' => 'admin3@test.com']
+        ))->create();
+
+        $admins->each(function ($admin) {
+            $admin->assignRole('admin');
+        });
     }
 }
