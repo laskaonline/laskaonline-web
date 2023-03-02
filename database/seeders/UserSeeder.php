f52b67aea8d77862b2f8519b95b010899956d0b0
Alvin Leonardo <alvinvin00@outlook.com>
2023-03-02 21:49:46 +0700

    chore: add 1 predefined user data

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,12 +3,15 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory(5);
+        User::factory(5)->state(new Sequence([
+            'email' => 'visitor@test.com'
+        ]));
     }
 }
