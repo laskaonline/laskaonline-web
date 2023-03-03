3455ce7f6f6196de06ac0b9d02b48bf598b1f177
Yudistira Sira Permana <yudistirasirapermana@gmail.com>
2023-03-04 06:16:35 +0700

    update : modify migration table

--- resources/views/user/registrasi-kunjungan.blade.php
+++ resources/views/user/registrasi-kunjungan.blade.php
@@ -12,7 +12,28 @@
 
     <div class="col-md-12 col-sm-12 ">
         <div class="x_panel">
+            <div class="card-box table-responsive">
 
+                <table id="datatable" class="table table-striped table-bordered" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Nama Barang</th>
+                            <th>Jumlah Barang</th>
+                            <th>Foto Barang</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td><input type="text" id="first-name" required="required" class="form-control "></td>
+                            <td><input type="text" id="first-name" required="required" class="form-control "></td>
+                            <td><input type="file" class="form-control" id="customFile" /></td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
