fa80c5e907caadb42bc9a8565eaa434aef7f3db4
Alvin Setiawan <49272368+sabo99@users.noreply.github.com>
2023-03-27 20:31:45 +0700

    add role name - API Show User

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
@@ -11,10 +12,11 @@ class UserController extends Controller
     public function show()
     {
         $user = Auth::user();
+        $user->role_name = $user->roles()->first()->value('name');
 
         return response()->json([
             'status' => 'success',
-            'data' => $user
+            'data' => $user,
         ]);
     }
 
