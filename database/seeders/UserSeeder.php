bd5ad3314805ebd8a8e3ba1b123004460e4a9e81
Alvin Setiawan <49272368+sabo99@users.noreply.github.com>
2023-03-28 06:18:31 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,8 +10,14 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory(5)->state(new Sequence([
-            'email' => 'visitor@test.com'
-        ]));
+        $visitors = User::factory(3)->state(new Sequence(
+            ['email' => 'visitor1@test.com'],
+            ['email' => 'visitor2@test.com'],
+            ['email' => 'visitor3@test.com']
+        ))->create();
+
+        $visitors->each(function ($visitor) {
+            $visitor->assignRole('visitor');
+        });
     }
 }
