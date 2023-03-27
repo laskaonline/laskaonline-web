2a9309793a1a3a64ff89dffc8a3b208a8cab901a
Yudistira Sira Permana <yudistirasirapermana@gmail.com>
2023-03-27 08:34:32 +0700

    test : symlink

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -31,7 +31,7 @@
                                 Pengunjung</label>
                             <div class="col-md-6 col-sm-6 ">
                                 @if ($user->photo !==null)
-                                    <img src="{{ asset('storage/pubic' . $user->photo) }}" class="img-thumbnail"
+                                    <img src="{{ asset('storage/pubic/' . $user->photo) }}" class="img-thumbnail"
                                          style="width:30%">
                                 @else
                                     <span class="badge badge-danger">No Foto</span>
