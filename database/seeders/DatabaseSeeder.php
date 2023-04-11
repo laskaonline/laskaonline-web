bfb2188c15f924407f4b57c41288fdb09c8a4e93
Alvin Setiawan <49272368+sabo99@users.noreply.github.com>
2023-04-11 21:31:51 +0700

    feat: migration, model & seeder user has transactions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-            AdminSeeder::class
+            AdminSeeder::class,
+            TransactionSeeder::class,
         ]);
     }
 }
