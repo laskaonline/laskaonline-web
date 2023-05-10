cfb10241d9ba3c04201b02176e1c3a520227092a
Alvin Leonardo <alvinvin00@outlook.com>
2023-05-10 21:15:13 +0700

    fix: Register API validation

--- app/Http/Requests/RegisterUserRequest.php
+++ app/Http/Requests/RegisterUserRequest.php
@@ -12,25 +12,24 @@ class RegisterUserRequest extends FormRequest
         return [
             'name' => ['required', 'string'],
             'no_ktp' => ['required', 'string', 'between:16,17'],
-            'email' => ['required', 'email', 'unique:users,email'],
-            'phone' => ['required', 'starts_with:08,+62'],
+            'email' => ['required', 'email', 'nullable'],
+            'phone' => ['required', 'starts_with:08,+62', 'unique:users,phone'],
             // 'password' => ['required', 'confirmed', Password::min(8)->uncompromised(3)],
             'password' => ['required', 'confirmed', Password::min(8)],
-            'password_confirmation' => ['required'],
         ];
     }
 
     public function messages(): array
     {
         return [
-            'required'    => ':Attribute harus diisi.',
-            'string'      => ':Attribute harus berupa string.',
-            'email'       => ':Attribute harus alamat email yang valid.',
-            'unique'      => ':Attribute sudah terdaftar.',
+            'required' => ':Attribute harus diisi.',
+            'string' => ':Attribute harus berupa string.',
+            'email' => ':Attribute harus alamat email yang valid.',
+            'unique' => ':Attribute sudah terdaftar.',
             'starts_with' => ':Attribute harus diawali dengan :values.',
-            'between'     => ':Attribute harus berjumlah :min sampai :max karakter.',
-            'confirmed'   => ':Attribute tidak cocok dengan konfirmasi :attribute.',
-            'min'         => ':Attribute harus berjumlah minimal :min karakter.',
+            'between' => ':Attribute harus berjumlah :min sampai :max karakter.',
+            'confirmed' => ':Attribute tidak cocok dengan konfirmasi :attribute.',
+            'min' => ':Attribute harus berjumlah minimal :min karakter.',
         ];
     }
 
